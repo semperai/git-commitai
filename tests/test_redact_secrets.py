9bc3728f67b320b16dbfb19618828a2429831431
Kasumi Null <kasumi.null@yandex.com>
2025-08-17 17:27:55 +0800

    Remove unnecessary sys.path modification in test file

--- tests/test_redact_secrets.py
+++ tests/test_redact_secrets.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-# Add parent directory to path
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import git_commitai
 
 class TestRedactSecrets:
